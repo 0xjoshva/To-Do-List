c5d3f266968d3f7448ccf748307b5608acb37914
Joshua Steed <joshuasteed123@gmail.com>
2022-06-23 09:47:35 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+//add new task
+function addNewTask() {
+  const newTask = document.querySelector("#taskInput").value;
+  document.querySelector(
+    ".container"
+  ).innerHTML += `<li class="item">${newTask}</li>`;
+}
+
+
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
